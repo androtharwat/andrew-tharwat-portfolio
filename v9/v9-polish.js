63fbd3b888cbb0517b3960b172e50a5d9b25c70b
androtharwat <101911752+androtharwat@users.noreply.github.com>
2026-09-25 12:10:38 +0300

    ATS identity: bump V10 runtime after logo fix

--- v9/v9-polish.js
+++ v9/v9-polish.js
@@ -20,7 +20,7 @@
   addCss('/v9/v9-home-v10-interactive.css?v=1');
   addCss('/v9/v9-home-v10-responsive.css?v=1');
   addCss('/v9/v9-home-first-impression.css?v=7');
-  addScript('/v9/v9-home-v10.js?v=13');
+  addScript('/v9/v9-home-v10.js?v=14');
 
   function applyOfficialBrandAssets(){
     const logo=$('.site-header .brand img');
